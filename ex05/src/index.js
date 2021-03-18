48348d866e965228cc76d8b7786527e7c9e699b4
EdinaDinka <cuturaedina@gmail.com>
2021-03-18 23:37:15 +0100

    Added ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,7 @@
+function myNestedFunction(arr) {
+    return arr;
+}
+var myNestedArray = [["Toblerone", 5], ["Milka", 3]];
+
+console.log(myNestedFunction(myNestedArray));
+module.exports = myNestedFunction;
