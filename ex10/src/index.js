dcfb3eadadb5f4bb3b50fb2e47148574c1ee45e6
EdinaDinka <cuturaedina@gmail.com>
2021-03-19 09:08:00 +0100

    Added ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+function mySplice(arr1, arr2, n) {
+    var array = [...arr2];
+    for (var i = 0; i < arr1.length; i++) {
+        array.splice(n, 0, arr1[i]);
+    }
+    return array;
+}
+
+console.log(mySplice([1, 2, 3], [4, 5], 1));
+console.log(mySplice([1, 2, 3], [4, 5], 2));
+console.log(mySplice(["b","c"], ["a", "d"], 1));
+console.log(mySplice(["Dell", "Phillips"], ["Samsung", "AOX", "Sony"], 2));
+module.exports = mySplice;
